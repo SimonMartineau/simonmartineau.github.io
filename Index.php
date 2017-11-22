e25ccd5bc6425738cea175e469b97f6c83172955
SimonMartineau <simon.peter.martineau@gmail.com>
2017-11-22 06:27:33 -0500

    Add files via upload
    Testing profile website features.

---
+++ Index.php
@@ -0,0 +1,89 @@
+<html>
+	<head >
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width=device-width">
+		<meta name = "description" content = "Secondlife Scripting services">
+		<meta name = "keywords" content = "Affordable Scripting, Secondlife, LSL, Small Business">
+		<meta name = "author" content = "Simon Martineau">
+		<title>BeyondScripting | Home</title>
+		<link rel="stylesheet" type="text/css" href="./CSS/BeyondScripting.css">
+	</head>
+	<body>
+		<header class = "dark rounded">
+			<h1>Beyond Scripting</h1>
+			<nav>
+				<a class = "button light" href = "Index.php">Home</a>
+				<a class = "button dark" href = "codeResume.php">Programmer</a>
+			</nav>
+		</header>
+		<section class = "backgroundpicture">
+			<div class = "center">
+				<h1>Beyond Scripting</h1>
+				<p>
+					Custom Scripts for your business needs.
+				</p>
+			<div>
+		</section>
+		<main class = "light">
+			<div class = "center">
+			<h2>Business Scripts</h2>
+			<p>for<p>
+			<section>
+				<h3>Computers</h3>
+				
+					<h4>Responsive Web Design</h4>
+						<p>
+							Tripple column design, navigation bar.
+						</p>
+					<h4>Business Programs</h4>
+						<p>
+							Python scripts to automate business tasks.
+						</p>
+					<h4>Portability</h4>
+						<p>
+							Windows and Linux
+						</p>
+			</section>
+			<section>
+				<h3>Tablets</h3>
+					<h4>Responsive Web Design.</h4>
+						<p>
+							Double Column design, Drop down menu.
+						</p>
+					<h4>Business Apps</h4>
+						<p>
+							Apps for your business and customers.
+						</p>
+					<h4>Portability</h4>
+						<p>
+							Android and IOS
+						</p>
+			
+			</section>
+			<section>
+				<h3>Phones</h3>
+					<h4>Responsive Web Design.</h4>
+						<p>
+							Single Column design, Drop down menu.
+						</p>
+					<h4>Business Apps</h4>
+						<p>
+							Apps for your business and customers.
+						</p>
+					<h4>Portability</h4>
+						<p>
+							Android and IOS
+						</p>
+			
+			</section>
+		</main>
+		</div>
+		<footer class = "dark">
+			<p>
+				contact us today at
+				<a class = "light" href="mailto:beyondscripting@gmail.com">
+				beyondscripting@gmail.com</a>
+			</p>
+		</footer>
+	</body>
+</html>
---
+++ codeResume.php
@@ -0,0 +1,95 @@
+<html>
+	<head >
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width=device-width">
+		<meta name = "description" content = "Secondlife Scripting services">
+		<meta name = "keywords" content = "Affordable Scripting, Secondlife, LSL, Small Business">
+		<meta name = "author" content = "Simon Martineau">
+		<title>BeyondScripting | Resume</title>
+		<link rel="stylesheet" type="text/css" href="./CSS/BeyondScripting.css">
+	</head>
+	<body>
+		<header class = "rounded dark">
+			<h1>Beyond Scripting</h1>
+			<nav>
+				<a class = "button dark" href = "Index.php">Home</a>
+				<a class = "button light" href = "codeResume.php">Programmer</a>
+			</nav>
+		</header>
+		
+		<section class = "backgroundpicture light">
+			<div class = "center">
+				<h2>Simon Martineau</h2>
+				<p>Small business programmer. Learned programming in LSL, taking English idea's and implementing them through computational and mathematical means. Problem Solver through modular design. </p>
+			</div>
+		</section>
+		
+		<section class = "light">
+		<div class = "center">
+			<h2>Developer Skills</h2>
+			<div class = "dualcolumn">
+				<h3>Programing Languages</h3>
+				<ul>
+					<li class = "trippleboxes">PHP</li>
+					<li class = "trippleboxes">CSS3</li>
+					<li class = "trippleboxes">Python</li>
+					<li class = "trippleboxes">MySQL</li>
+					<li class = "trippleboxes">HTML5</li>
+					<li class = "trippleboxes">C++</li>
+					<li class = "trippleboxes">Javascript</li>
+				</ul>
+			</div>
+			<div class = "dualcolumn">
+				<h3>Design</h3>
+				<ul>
+					<li>Problem Solving</li>
+					<li>Learning</li>
+					<li>Research</li>
+					<li>Program Design</li>
+				</ul>
+			</div>
+			
+		</div>
+		</section>
+		
+		<section class = "dark">
+			<div class = "center">
+				<h2>Remote</h2>
+				<p>Only interested in remote positions.</p>
+			</div>
+		</section>
+		
+		<section class = "light">
+			<div class = "center">
+			<h2>Projects</h2>
+			<ul>
+				<li>Github</li>
+			</ul>
+			</div>
+		</section>
+		
+		<section class = "dark">
+			<div class = "center">
+			<h2>Experience</h2>
+			<p>2008-2014 LSL Scripter</p>
+			<p>2014-2015 Computer Program Analyst St Lawrence</p>
+			<p>2017-???? Beyond Scripting</p>
+			</div>
+		</section>
+		
+		<section class = "light">
+			<div class = "center">
+			<h2>Contact</h2>
+			<p></p>
+			</div>
+		</section>
+		
+		<footer class = "dark">
+			<p>
+				contact us today at
+				<a class = "light" href="mailto:beyondscripting@gmail.com">
+				beyondscripting@gmail.com</a>
+			</p>
+		</footer>
+	</body>
+</html>
